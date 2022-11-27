47668fbca164e4f147b76a072bc8137f0e7352ed
root <lj@vipex.cc>
2022-11-27 20:11:30 +0800

    完善 yml 获取

--- util/getarchives/init.go
+++ util/getarchives/init.go
@@ -4,20 +4,28 @@ import (
 	"os"
 	"encoding/json"
 	"strings"
+	"go-deploy/app"
+	//"fmt"
 )
 
-var c []struct {Sha string}
+var c []struct {
+	Sha string `json:"sha"`
+	DownloadUrl string `json:"download_url"`
+	Name string `json:"name"`
+}
 
 func Zip() {
 	for _, rep := range strings.Split(os.Getenv("rep"), ",") {
 			p := strings.Split(rep, "*")
 			// https://api.github.com/repos/juju-l/go-deploy/commits?sha=dev&per_page=1&path=/config
-			rsp,_ := req().Get(""+"repos/"+os.Getenv("usr")+"/"+p[0]+"/commits"+"?sha="+p[2]+"&per_page=1&path="+p[1]+"") /**/
-			/*e := */json.Unmarshal(rsp.Body(), &c)
-			//\string([]rune(c[0].Sha)[:7])
-			// https://api.github.com/repos/juju-l/go-deploy/contents/config?ref=a34126c
+			commits,_ := req().Get(""+"repos/"+os.Getenv("usr")+"/"+p[0]+"/commits"+"?sha="+p[2]+"&per_page=1&path="+p[1]+"") /**/
+			/*e := */json.Unmarshal(commits.Body(), &c)
+			sha := string([]rune(c[0].Sha)[:7])
 			// https://api.github.com/repos/owner/rep/zipball/p[2](sha)
-			//apply
+			// https://api.github.com/repos/juju-l/go-deploy/contents/config?ref=a34126c
+			content,_ := req().Get(""+"repos/"+os.Getenv("usr")+"/"+p[0]+"/contents"+p[1]+"?"+"ref="+sha+"") /**/
+			/*e := */json.Unmarshal(content.Body(), &c)
+			app.Apply(get())
 			// check status ...
 	}
 }
--- util/getarchives/r.go
+++ util/getarchives/r.go
@@ -1,1 +1,17 @@
 package getarchives
+
+//
+///
+func get() map[string]interface{} {
+	var f = make(map[string]interface{})
+	for _, o := range c {
+		if o.DownloadUrl != "" {
+			r, e := req().Get(o.DownloadUrl);if e!=nil { continue };f[o.Name] = /*string(*/r.Body()/*)*/
+		}
+	}
+	return f
+}
+
+func init() {
+	//
+}
