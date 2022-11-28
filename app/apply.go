5c98e2b52c91e0e898c959641f8a8a9b51ad707e
root <lj@vipex.cc>
2022-11-28 17:31:23 +0800

    complete

--- app/apply.go
+++ app/apply.go
@@ -2,36 +2,51 @@ package app
 
 import (
 	"bytes"
+	"context"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/restmapper"
-	"context"
+	//"fmt"
 )
 
 var cli, _ = dynamic.NewForConfig(getConfig())
 
 func Apply(f map[string]interface{}) {
-	d := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(f["k8sic.yml"].([]byte)), 768) //
-	for {
-			resources, _ := restmapper.GetAPIGroupResources(getDiscover())
-			mapper := restmapper.NewDiscoveryRESTMapper(resources)
-			var rawObjc runtime.RawExtension; if err := d.Decode(&rawObjc); err != nil { break }; // exit
-			///"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
-			///obj, gvk, err := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme).Decode(rawObjc.Raw, nil, nil)
-			obj, gvk, err := unstructured.UnstructuredJSONScheme.Decode(rawObjc.Raw, nil, nil)
-			// fmt.Println(obj); fmt.Println(gvk); fmt.Println(err)
-			mping, err := mapper.RESTMapping(/**/ gvk.GroupKind(), gvk.Version)
-			// cli.Resource(mping.Resource).Apply(context.Background(), name, obj)
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(
+			memory.NewMemCacheClient(getDiscover()),
+		)
+	for _,v := range f {
+			d := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(v.([]byte)), 768) //
+					for {
+			var r runtime.RawExtension; if err := d.Decode(&r); err != nil { break }; // exit
+			o := &unstructured.Unstructured{}
+			_, gvk, _ := unstructured.UnstructuredJSONScheme.Decode(r.Raw, nil, o)
+			m, _ := mapper.RESTMapping(/**/ gvk.GroupKind(), gvk.Version)
+			var dri dynamic.ResourceInterface
+			dri = cli.Resource(m.Resource)
+			if m.Scope.Name() == meta.RESTScopeNameNamespace {
+					dri = cli.Resource(m.Resource).Namespace(o.GetNamespace())
+			}
+			/*_, err :=*/ dri.Apply(
+			context.Background(), o.GetName(), o, v1.ApplyOptions{} /*otherRes*/,
+			)
+					}
+			// ----------------------------------- ---
 	}
-	///
+	//
+	//\ get deploy sta
+	//
 }
 
 func init() {
 	//
 	///
-	//\\\\\
+	//\\\\\\\\\\\\\\\\\\\\\\\\\
 	/////
 	//\
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module go-deploy
 require (
 	k8s.io/client-go v0.25.3
 	github.com/go-resty/resty/v2 v2.7.0
-	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/apimachinery v0.25.3
 )
 
 go 1.16
